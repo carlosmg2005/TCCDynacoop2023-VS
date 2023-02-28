c4fa02bf9c10cbc0c9832b9b7d9639bdf48ee895
Gabriel Coimbra De Oliveira Da Silva <54251679+coimbrox@users.noreply.github.com>
2023-02-28 17:19:40 -0300

    Configurando Ambiente de Desenvolvimento

---
+++ Tcc.Dynacoop.Equipe4.Plugin/Actions/BuscaCEPApi.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tcc.Dynacoop.Equipe4.Plugin.DynacoopISV;
+
+namespace Tcc.Dynacoop.Equipe4.Plugin.Actions
+{
+    public class BuscaCEPApi : ActionCore
+    {
+
+        [Input("Cep")]
+
+        public InArgument<string> Cep { get; set; }
+
+
+        public override void ExecuteAction(CodeActivityContext context)
+        {
+
+        }
+    }
+}
---
+++ Tcc.Dynacoop.Equipe4.Plugin/Plugins/ProductManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tcc.Dynacoop.Equipe4.Plugin.DynacoopISV;
+
+namespace Tcc.Dynacoop.Equipe4.Plugin
+{
+    public class ProductManager : PluginCore
+    {
+              public override void ExecutePlugin(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
