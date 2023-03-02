84a7fc6fdce3fb319a0896b74a1578640b1cbd6a
Anderson Belo Delfino <anderbd22@gmail.com>
2023-03-02 18:03:00 -0300

    Validação de duplicidade do CNPJ

---
+++ Tcc.Dynacoop.Equipe4.SharedProject/Controller/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using Tcc.Dynacoop.Equipe4.SharedProject.Model;
+
+namespace Tcc.Dynacoop.Equipe4.SharedProject.Controller
+{
+    public class AccountController
+    {
+        public IOrganizationService ServiceClient { get; set; }
+
+        public Account Account { get; set; }
+
+        public AccountController(IOrganizationService crmnServiceClient)
+        {
+            this.ServiceClient = crmnServiceClient;
+            this.Account = new Account(ServiceClient);
+        }
+
+        public AccountController(CrmServiceClient crmnServiceClient)
+        {
+            this.ServiceClient = crmnServiceClient;
+            this.Account = new Account(ServiceClient);
+        }
+
+        public Entity GetAccountByCnpj(string cep)
+        {
+            return Account.GetAccountByCNPJ(cep);
+        }
+    }
+}
---
+++ Tcc.Dynacoop.Equipe4.SharedProject/Model/Account.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System.Linq;
+using System;
+
+namespace Tcc.Dynacoop.Equipe4.SharedProject.Model
+{
+    public class Account
+    {
+        public IOrganizationService ServiceClient { get; set; }
+
+        public string LogicalName { get; set; }
+
+        public Account(IOrganizationService crmServiceClient)
+        {
+            this.ServiceClient = crmServiceClient;
+            this.LogicalName = "account";
+        }
+
+        public Account(CrmServiceClient crmServiceClient)
+        {
+            this.ServiceClient = crmServiceClient;
+            this.LogicalName = "account";
+        }
+
+        private Entity RetrieveOneAccount(QueryExpression queryAccount)
+        {
+            EntityCollection contas = this.ServiceClient.RetrieveMultiple(queryAccount);
+            if (contas.Entities != null || contas.Entities.Count > 0)
+                return contas.Entities.FirstOrDefault();
+            else
+                Console.WriteLine("Nenhuma conta encontrada com esse nome");
+
+            return null;
+        }
+
+        public Entity GetAccountByCNPJ(string cnpj)
+        {
+            QueryExpression queryAccount = new QueryExpression(this.LogicalName);
+            queryAccount.ColumnSet = new ColumnSet(true);
+            queryAccount.Criteria.AddCondition("dnc_cnpj", ConditionOperator.Equal, cnpj);
+
+            return RetrieveOneAccount(queryAccount);
+        }
+    }
+}
