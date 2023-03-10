0b227c4a898c29a68b3c010e3ee10ca0da63e678
Anderson Belo Delfino <anderbd22@gmail.com>
2023-03-10 17:24:06 -0300

    Alteração do arquivo JS

---
+++ Tcc.Dynacoop.Equipe4.Customization/Javascript/BloqueiaEdicaoCPF.js
@@ -0,0 +1,15 @@
+﻿if (typeof (Dynacoop) == "undefined") { Dynacoop = {} }
+if (typeof (Dynacoop.BlockCpf) == "undefined") { Dynacoop.BlockCpf = {} }
+
+Dynacoop.BlockEditionCpf = {
+    OnLoad: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+
+        var cpf = formContext.getAttribute("dnc_cpf").getValue();
+        if (cpf != null) {
+            formContext.getControl("dnc_cpf").setDisabled(true);
+        } else {
+            formContext.getControl("dnc_cpf").setDisabled(false);
+        }
+    }
+}
